4a10272369059022b8963b2cb23e0f890545f9de
ipolito42 <phypolitopoulos@gmail.com>
2019-04-13 22:13:50 +0200

    evolution working if this bug is fixed

--- main.py
+++ main.py
@@ -10,71 +10,78 @@ from amuse.community.huayno.interface import Huayno
 # from amuse.datamodel import Particles, Particle
 
 
+number_of_binaries = 5
+
 smbh = SuperMassiveBlackHole(mass=1e7 | units.MSun)
 smbh_mass = smbh.mass
-
-blackhole_masses = np.random.uniform(low=10, high=15, size=2)
 
 inner_boundary = (smbh.radius.value_in(units.parsec)) * 100
 outer_boundary = (smbh.radius.value_in(units.parsec)) * 100000
 
-binaries = BinaryBlackHole(blackhole_masses[0], blackhole_masses[1], smbh_mass,
-                         initial_outer_semi_major_axis=np.random.uniform(inner_boundary, outer_boundary, size=1)[0] | units.parsec,
-                         initial_outer_eccentricity=np.random.uniform(0, 0.99, size=1)[0],
-                         eccentricity=np.random.uniform(0.0, 0.99, size=1),
-                         inclincation=np.random.uniform(0.0, 180.0, size=1),
-                         )
 
 
-all_particles = Particles()
-all_particles.add_particle(smbh.super_massive_black_hole)
-all_particles.add_particles(binaries.blackholes)
+all_gravity_particles = Particles()
+
+for i in range(number_of_binaries):
+
+    blackhole_masses = np.random.uniform(low=10, high=15, size=2)
+
+    binaries = BinaryBlackHole(blackhole_masses[0], blackhole_masses[1], smbh_mass,
+                             initial_outer_semi_major_axis=np.random.uniform(inner_boundary, outer_boundary, size=1)[0] | units.parsec,
+                             initial_outer_eccentricity=np.random.uniform(0, 0.99, size=1)[0],
+                             eccentricity=np.random.uniform(0.0, 0.99, size=1),
+                             inclincation=np.random.uniform(0.0, 180.0, size=1),
+                             )
+    all_gravity_particles.add_particles(binaries.blackholes)
+
+
+all_gravity_particles.add_particle(smbh.super_massive_black_hole)
+
 
 converter = nbody_system.nbody_to_si(smbh_mass, 100000 * smbh.radius)
-print ('smbh radius: ', smbh.radius.in_(units.parsec))
 gravity = Huayno(converter)
+gravity.particles.add_particles(all_gravity_particles)
+
+channel_from_grav_to_binaries = gravity.particles.new_channel_to(all_gravity_particles)
+channel_from_binaries_to_grav = all_gravity_particles.new_channel_to(gravity.particles)
 
 
-gravity.particles.add_particle(all_particles)
+
+
+
+
+# ----------------------- must become parameter -----------------------#
+end_time = 5 | units.Myr
+timestep = 0.1 | units.Myr
+# ---------------------------------------------------------------------#
+sim_time = 0. | units.Myr
+
+while sim_time < end_time:
+    sim_time += timestep
+    print('lego')
+    gravity.evolve_model(sim_time)
+    print('letsgo')
+
+    channel_from_grav_to_binaries.copy()
+
+gravity.stop()
+
+
 print(gravity.particles)
 print('initial outer semi major axis: ', binaries.initial_outer_semi_major_axis.in_(units.AU), \
             '\nbinaries hill radius: ', binaries.hill_radius.in_(units.AU), \
             '\ninitial outereccentricity: ', binaries.initial_outer_eccentricity, \
             '\nbinaries max orbital period: ', binaries.binary_max_orbital_period.in_(units.yr), \
             '\nbinaries min orbital period: ', binaries.binary_min_orbital_period.in_(units.yr), \
-            '\nblackhole1 position: ', binaries.blackholes[0].position.in_(units.AU), \
-            '\nblackhole2 position: ', binaries.blackholes[1].position.in_(units.AU), \
+            '\nbinary blackholes position: ', binaries.blackholes.position.in_(units.AU), \
             '\ntotal binary mass: ', binaries.total_mass.in_(units.MSun), \
             '\nsmbh mass: ', binaries.central_blackhole.mass.in_(units.MSun), \
             '\nbinary blackhole mass: ', binaries.blackholes.mass.in_(units.MSun), \
-            '\binary blackhole distance: ', binaries.blackholes_distance.in_(units.AU))
-
-# channel_from_grav_to_binaries = gravity.particles.new_channel_to(all_particles)
-
-
-# gravity.stop()
+            '\nbinary blackhole distance: ', binaries.blackholes_distance.in_(units.AU))
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-#
 # from AccretionDisk import AccretionDisk
 # from SuperMassiveBlackHole import SuperMassiveBlackHole
 # from BinaryBlackHole import BinaryBlackHole
