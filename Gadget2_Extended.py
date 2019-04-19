15b199b9077b73667d081f202b91712914b0966c
Jacob Bieker <jacob@bieker.tech>
2019-04-19 18:06:30 +0200

    Add get potential at point and remove debug statements

--- Gadget2_Extended.py
+++ Gadget2_Extended.py
@@ -8,39 +8,13 @@ class Gadget2_Extended(Gadget2):
         Gadget2.__init__(self, unit_converter=unit_converter, mode=mode, **options)
         self.radius_for_gravity_calc = radius
 
-    def get_gravity_at_point(self, alpha, x, y, z):
-        print("Start Field Code", flush=True)
+    def get_gravity_at_point(self, radius, x, y, z):
         field_code = CalculateFieldForParticles(particles=self.gas_particles, G=constants.G)
-        print("Made Field Code", flush=True)
-        #print(field_code.get_gravity_at_point(alpha, x, y, z))
-        return field_code.get_gravity_at_point(alpha, x, y, z)
-        '''
-        if self.radius_for_gravity_calc is None:
-            self.radius_for_gravity_calc = 2. | x.unit
-        center_point = np.asarray([x.value_in(x.unit), y.value_in(x.unit),z.value_in(x.unit)]) | x.unit
-        nearby_particles = self.gas_particles.select(
-            lambda r: np.abs((r - center_point).length()) < self.radius_for_gravity_calc, ["position"])
+        return field_code.get_gravity_at_point(radius, x, y, z)
 
-        if len(nearby_particles) <= 0:
-            # No particles close enough to affect it
-            return np.asarray([[0.0], [0.0], [0.0]]) | units.m / (units.s*units.s)
-        print(len(nearby_particles), flush=True)
-
-        # Now get center of mass of the particles
-        # Then calculate acceleration from that "particle" to the point
-
-        center_of_mass = nearby_particles.center_of_mass()
-        total_mass = nearby_particles.mass.sum()
-
-        # GMm/r^2
-        x_y_z = [center_of_mass[0].value_in(center_of_mass.unit)-center_point[0].value_in(center_of_mass.unit),
-                 center_of_mass[1].value_in(center_of_mass.unit)-center_point[1].value_in(center_of_mass.unit),
-                 center_of_mass[2].value_in(center_of_mass.unit)-center_point[2].value_in(center_of_mass.unit)] | center_of_mass.unit
-        print(x_y_z, flush=True)
-        ax,ay,az = (constants.G*total_mass)/((x_y_z)**2)
-        print(ax, flush=True)
-        return ax,ay,az
-        '''
+    def get_potential_at_point(self, radius, x, y, z):
+        field_code = CalculateFieldForParticles(particles=self.gas_particles, G=constants.G)
+        return field_code.get_potential_at_point(radius, x, y, z)
 
     def add_particles(self, particles):
         self.gas_particles.add_particles(particles)
