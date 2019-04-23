15baf30dc068645e7f10ed5a4ab44b28419bcdbf
Jacob Bieker <jacob@bieker.tech>
2019-04-23 11:39:00 +0200

    Add more binaries

--- Jacob_main.py
+++ Jacob_main.py
@@ -12,9 +12,9 @@ from BinaryBlackHolesWithAGN import BinaryBlackHolesWithAGN
 
 
 simulation = BinaryBlackHolesWithAGN(mass_of_central_black_hole=1e6 | units.MSun,
-                                     number_of_binaries=1,
+                                     number_of_binaries=50,
                                      number_of_gas_particles=1000000,
                                      disk_mass_fraction=0.1,
-                                     number_of_hydro_workers=3,
-                                     number_of_grav_workers=1,
-                                     end_time=50 | units.Myr)
+                                     number_of_hydro_workers=2,
+                                     number_of_grav_workers=2,
+                                     end_time=100 | units.Myr)
